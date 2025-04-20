e645dbe7bb8d5f29125dfa240e6909231bd3ffec
Amritanshu Singh <amritanshusk2610129@gmail.com>
2025-04-20 17:18:48 +0530

    added expplaination

--- NUMPY/2slicing.py
+++ NUMPY/2slicing.py
@@ -1,1 +1,6 @@
 import numpy as np
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+print(arr[0:2, 1:4])
+#meaning of this is that it select first 2 elements 
+#from arr and 1:4 means that it select next 3 elements fron index 1
