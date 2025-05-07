41ec15a23a9398c8705b5831f5541902889e4839
Amritanshu Singh <amritanshusk2610129@gmail.com>
2025-05-07 20:35:10 +0530

    added some new stuf

--- NUMPY/6arrayreshape.py
+++ NUMPY/6arrayreshape.py
@@ -11,3 +11,24 @@ print(newarr.ndim)
 arr=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 newarr=arr.reshape((2,3,2))
 print(newarr)
+
+# is arr.reshape is view or copy of the original array ???
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+u=arr.reshape(4,2)
+print(u)            
+print(u.base)  # answer is it is just a view of the original array 
+# as the output is [1,2,3,4,5,6,7,8]
+
+
+# important thing to notice 
+'''changes  made to view also affect original array'''
+
+exapl=np.array([1,2,3,4,5,6,7,8])
+view=exapl.reshape(2,4)
+
+view[0][0]=100
+
+print('view array:',view)
+print('origianl:',exapl)
